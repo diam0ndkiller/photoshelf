ad38cd8ae28a2263daecd4a2a9df215661d4f78c
diam0ndkiller <>
2025-07-22 19:03:31 +0200

    implemented basic http auth for dev frontent

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -1,22 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import httpAuth from 'vite-plugin-http-basic-auth';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return ({
+    plugins: [
+      vue(),
+      vueDevTools(),
+      httpAuth(
+        [
+          {
+            username: 'diam0ndkiller',
+            password: env.VITE_AUTH_PASSWORD
+          }
+        ],
+        {realm: '/'}
+      ),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    port: 8090,
-    host: '0.0.0.0',
-  }
+    server: {
+      port: 8090,
+      host: '0.0.0.0',
+    }
+  })
 })
