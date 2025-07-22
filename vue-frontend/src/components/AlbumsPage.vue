3786abede7151cb43e125f8bdec5b33844057f54
diam0ndkiller <>
2025-07-22 19:04:57 +0200

    added basic testing ui for album overview

---
+++ vue-frontend/src/components/AlbumsPage.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import BackendHandler from '@/utils/backendHandler';
+
+const albums = (await BackendHandler.listAlbums()).albums;
+</script>
+
+<template>
+    <v-main>
+        <div class="page-content">
+            <h1>{{ currentPath }}</h1>
+            <ul>
+                <li v-for="album in albums">{{ album.name }}</li>
+            </ul>
+        </div>
+    </v-main>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    },
+    methods: {
+        updatePath(newPath: string) {
+            this.$emit('updatePath', newPath);
+        }
+    },
+    props: {
+        currentPath: {
+            type: String,
+        }
+    },
+    emits: ['updatePath']
+}
+</script>
+
+<style>
+
+</style>
