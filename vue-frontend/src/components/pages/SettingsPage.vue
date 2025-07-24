1371cd89d1a1d8552c6ab713c425ea9cd0bcf89e
diam0ndkiller <>
2025-07-24 18:47:01 +0200

    added settings page with about section

---
+++ vue-frontend/src/components/pages/SettingsPage.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import AboutPhotoshelf from '../subcomponents/AboutPhotoshelf.vue';
+</script>
+
+<template>
+<v-main>
+    <div class="page-content">
+        <AboutPhotoshelf/>
+    </div>
+</v-main>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    },
+    methods: {
+        updatePath(newPath: string) {
+            this.$emit('updatePath', newPath);
+        }
+    },
+    props:{
+        currentPath: {
+            type: String
+        }
+    },
+    emits: ['updatePath']
+}
+</script>
+
+<style>
+
+</style>
