3dda3dd657f69e4e7f12a3d5378a47972651bbc6
diam0ndkiller <>
2026-01-20 18:16:21 +0100

    add very basic album information page to view instead of overview when selected from sidebar

--- vue-frontend/src/components/pages/AlbumsPage.vue
+++ vue-frontend/src/components/pages/AlbumsPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BackendHandler from '@/utils/backendHandler';
+import Album from '../subcomponents/Album.vue';
 
 const albums = (await BackendHandler.listAlbums()).albums;
 </script>
@@ -7,7 +8,8 @@ const albums = (await BackendHandler.listAlbums()).albums;
 <template>
     <v-main>
         <div class="page-content">
-            <div>
+            <Album v-if="inAlbum" :id="albumId"/>
+            <div v-else>
                 <h1 style="display: inline;">All Albums</h1>
                 <div style="display: inline-block; margin: 2%"><v-btn color="accent" prepend-icon='mdi-plus-circle-outline' text='New Album' @click="toggleAddAlbum()"/></div>
                 <span style="color: rgb(var(--v-theme-accent))">{{ statusMessage }}</span>
@@ -30,7 +32,16 @@ export default {
         }
     },
     computed: {
-        
+        inAlbum(): boolean {
+            if (this.currentPath.includes("/", 2)) return true;
+            else return false;
+        },
+        albumId(): Number {
+            if (this.inAlbum) {
+                var pathElements = this.currentPath.split("/");
+                return Number(pathElements[pathElements.length - 1]);
+            } else return -1;
+        }
     },
     watch: {
         
---
+++ vue-frontend/src/components/subcomponents/Album.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import BackendHandler from '@/utils/backendHandler';
+
+
+</script>
+
+<template>
+Viewing Album {{ albumInformation.name }} with id {{ albumInformation.id }}
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            albumInformation: {id: -1, name: "loading..."},
+        }
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    },
+    methods: {
+        
+    },
+    props:{
+        id: {
+            type: Number,
+            required: true,
+        }
+    },
+    async mounted() {
+        this.albumInformation = (await BackendHandler.getAlbumInformation(this.id)).album;
+    },
+    emits: []
+}
+</script>
+
+<style>
+
+</style>
