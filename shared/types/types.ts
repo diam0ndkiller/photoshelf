76cccbc91b778edfde92265a4602c5109d949362
diam0ndkiller <>
2026-02-08 12:02:40 +0100

    add shared type defintions file

---
+++ shared/types/types.ts
@@ -0,0 +1,24 @@
+export interface ErrorResultObject {
+    err: Error | { message: string }
+}
+
+export interface BackendConfig {
+    credentials: BackendConfigCredentials
+    databaseLocation: string
+}
+
+export interface BackendConfigCredentials {
+    [key: string]: string
+}
+
+export interface SuccessObject {
+    success: boolean
+}
+
+export interface TrueSuccessObject extends SuccessObject {
+    success: true;
+}
+
+export interface FalseSuccessObject extends SuccessObject {
+    success: false;
+}
