342f03b3cc79d1ae3af5fea44569dd74850d1c78
diam0ndkiller <>
2026-02-09 12:22:00 +0100

    move AlbumSheet view into Album component

--- vue-frontend/src/components/subcomponents/Album.vue
+++ vue-frontend/src/components/subcomponents/Album.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { BackendHandler } from '@/utils/backendHandler';
 import type { Album as AlbumType, JoinedAlbumContentLink } from '@shared/databasetypes';
-import AlbumSheet from './AlbumSheet.vue';
+import Photo from './Photo.vue';
 </script>
 
 <template>
@@ -15,8 +15,12 @@ import AlbumSheet from './AlbumSheet.vue';
             <v-color-picker/>
         </v-menu>
     </div>
-    
-    <AlbumSheet :color="albumInformation.background_color" :contents="pages(albumContents)[0]"/>
+
+    <div class="box-wrapper">
+        <div class="page-box" :style="`background-color: ${albumInformation.background_color};`">
+
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -25,16 +29,39 @@ export default {
         return {
             albumInformation: {} as AlbumType,
             albumContents: [] as JoinedAlbumContentLink[],
+            page: 0,
+            albumsList: [] as AlbumType[],
         }
     },
     computed: {
-        
+        paddedAlbumContents() {
+            const result = [...this.albumContents]
+            console.log(result);
+            while (result.length < 4 || result.length % 4 != 0) {
+                result.push({album_id: this.albumInformation.id,
+                            type: 'spacer',
+                            index: result.length,})
+            }
+            return result
+        },
+        albumsListWithoutSelf() {
+            const result: Array<AlbumType> = [];
+            this.albumsList.forEach(element => {
+                if (element.id != this.albumInformation.id) result.push(element);
+            });
+            return result
+        },
     },
     watch: {
         
     },
     methods: {
-        pages(contents: Array<JoinedAlbumContentLink>) {
+        async updateData() {
+            this.albumInformation = (await BackendHandler.getAlbumInformation(this.id)).album;
+            this.albumContents = (await BackendHandler.getAlbumContents(this.id)).contents;
+            this.albumsList = (await BackendHandler.listAlbums()).albums;
+        },
+        getPages(contents: Array<JoinedAlbumContentLink>) {
             var res = [];
 
             for (let i = 0; i < contents.length; i += 4) {
@@ -51,13 +78,22 @@ export default {
         }
     },
     async mounted() {
-        this.albumInformation = (await BackendHandler.getAlbumInformation(this.id)).album;
-        this.albumContents = (await BackendHandler.getAlbumContents(this.id)).contents;
+        this.updateData();
     },
     emits: []
 }
 </script>
 
 <style>
+.box-wrapper {
+  display: flex;
+  height: 100%;
+  justify-content: center; /* horizontal center */
+  align-items: center;     /* vertical center */
+}
 
+.page-box {
+  width: min(80vw, 80vh*(4/3));
+  height: min(80vh, 80vw*(3/4));
+}
 </style>
